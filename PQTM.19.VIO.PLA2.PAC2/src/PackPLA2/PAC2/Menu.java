e06170bbe197be6e948fb3a7117b100045b00e81
PortatilMIMO <PortatilMIMO@ACER-WINDOWS10>
2019-09-18 02:03:02 +0200

    V1.3

--- PQTM.19.VIO.PLA2.PAC2/src/PackPLA2/PAC2/Menu.java
+++ PQTM.19.VIO.PLA2.PAC2/src/PackPLA2/PAC2/Menu.java
@@ -1,9 +1,5 @@
 package PackPLA2.PAC2;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Scanner;
 
 public class Menu {
@@ -16,9 +12,6 @@ public class Menu {
 		
 	public void loop(Scanner reader) {	
 	
-		Date fechaDesde = new Date();
-		Date fechaHasta = new Date();
-		
 	    while (true) {
 	    	System.out.println("Entrar la fecha como ddMMyyyy o Quit: ");
 			String fecha = reader.nextLine();
@@ -29,37 +22,7 @@ public class Menu {
 			
 			boolean todobien = Validaciones.fechaEsValida(fecha);
 			if (todobien) {
-				System.out.println("Fecha correcta");
-		
-				DateFormat df = new SimpleDateFormat("ddMMyyyy");
-				
-				try {
-					fechaDesde = df.parse("01012019");
-					fechaHasta = df.parse(fecha);
-					System.out.println("Fecha desde " + df.format(fechaDesde));
-					System.out.println("Fecha Hasta " + df.format(fechaHasta));
-				} catch (ParseException e) {
-					e.printStackTrace();
-				}
-				
-				boolean fechaCorrecta = Validaciones.fechaMayorFechaInicial(fechaHasta);
-				System.out.println("Fecha correcta --> " + fechaCorrecta);
-				
-				int dias = Validaciones.numeroDiasEntreDosFechas(fechaDesde, fechaHasta);
-				System.out.println("Dias de diferencia: " + dias);
-
-				String fechaCadena = df.format(fechaHasta);
-
-				int diaint = Integer.parseInt(fechaCadena.substring(0, 2));
-				int mesint = Integer.parseInt(fechaCadena.substring(2, 4));
-				int anyoint = Integer.parseInt(fechaCadena.substring(4, 8));
-				
-				System.out.println("La fecha " + df.format(fechaHasta) + 
-						" es " +
-						DiaSemana.diaDeLaSemana(diaint, mesint, anyoint));
-	
-				break;
-				
+				 break;
 			} else {
 				System.out.println("Fecha incorrecta. Vuelva a informarla");
 			}
--- PQTM.19.VIO.PLA2.PAC2/src/PackPLA2/PAC2/Validaciones.java
+++ PQTM.19.VIO.PLA2.PAC2/src/PackPLA2/PAC2/Validaciones.java
@@ -1,5 +1,6 @@
 package PackPLA2.PAC2;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -17,28 +18,50 @@ public class Validaciones {
             return false;
         }
    
-        return true;
+        return fechaMayorFechaInicial(fecha);
+      //  return true;
     }
 	
-	public static boolean fechaMayorFechaInicial(Date fechaHasta) {
-		SimpleDateFormat df = new SimpleDateFormat("ddMMyyyy");
+	public static boolean fechaMayorFechaInicial(String fecha) {
+		
+		DateFormat df = new SimpleDateFormat("ddMMyyyy");
+		Date fechaDesde = new Date();
+		Date fechaHasta = new Date();
+		
 		try {
-			Date fechaDesde = df.parse("01011978");
+			fechaDesde = df.parse("01011978");
+			fechaHasta = df.parse(fecha);
+			System.out.println("Fecha desde " + df.format(fechaDesde));
+			System.out.println("Fecha Hasta " + df.format(fechaHasta));
 			int resultComparacion = fechaDesde.compareTo(fechaHasta);
-			if (resultComparacion > 0) {
+			if (resultComparacion >= 0) {
 				return false;
 			}
 		} catch (ParseException e) {
 			return false;
 		}
+
+		numeroDiasEntreDosFechas(fechaDesde, fechaHasta);
+		
+		desglosarFechaHasta(fecha);
+		
 		return true;
 	}
 	
-	public static int numeroDiasEntreDosFechas(Date fecha1, Date fecha2) {
+	public static void numeroDiasEntreDosFechas(Date fecha1, Date fecha2) {
 	     long startTime = fecha1.getTime();
 	     long endTime = fecha2.getTime();
 	     long diffTime = endTime - startTime;
 	     int totalDias = (int)TimeUnit.DAYS.convert(diffTime, TimeUnit.MILLISECONDS);
-	     return (totalDias + 1);
+	     System.out.println("Dias de diferencia: " + (totalDias + 1));
+	}
+	
+	public static void desglosarFechaHasta(String fecha) {
+		  int diaint = Integer.parseInt(fecha.substring(0, 2)); 
+		  int mesint = Integer.parseInt(fecha.substring(2, 4));
+		  int anyoint = Integer.parseInt(fecha.substring(4, 8));		
+		
+		  System.out.println("La fecha " + fecha + " es " +
+			DiaSemana.diaDeLaSemana(diaint, mesint, anyoint));
 	}
 }
